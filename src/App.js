47226d20a882732d6fc9c8be815fd57c5b7586dd
Ravi Mashru <mashru.ravi@gmail.com>
2020-04-08 08:24:22 +0530

    WIP: detect items in video feed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useRef, useEffect} from 'react';
 import './App.css';
+import * as cocoSsd from '@tensorflow-models/coco-ssd';
 
 function App() {
+  // Load the model
+  const modelPromise = cocoSsd.load({base: 'mobilenet_v2'});
+
+  // References for video and canvas elements
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    // Only video is required from the user's device
+    const mediaConstraints = {
+      audio: false,
+      video: {facingMode: 'environment'},
+    };
+
+    if (navigator.mediaDevices.getUserMedia) {
+      const videoStreamPromise = navigator.mediaDevices
+        .getUserMedia(mediaConstraints)
+        .then((stream) => {
+          videoRef.current.srcObject = stream;
+          // Wait for video element to load with data
+          return new Promise((resolve) => (videoRef.current.onloadedmetadata = resolve));
+        })
+        .catch(() => {
+          alert('Activate your camera and refresh the page.');
+        });
+
+      // Start prediction when the model and video stream have both loaded
+      Promise.all([modelPromise, videoStreamPromise])
+        .then((values) => runInference(videoRef.current, values[0]))
+        .catch((error) => console.error(error));
+    }
+  });
+
+  // Run inference on the video stream
+  const runInference = (video, model) => {
+    model.detect(video).then((predictions) => {
+      console.log(predictions);
+    });
+    setTimeout(() => {
+      runInference(video, model);
+    }, 1000);
+  };
+
+  // Draw bounding boxes
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <video ref={videoRef} autoPlay></video>
+      <canvas ref={canvasRef}></canvas>
+    </>
   );
 }
 
